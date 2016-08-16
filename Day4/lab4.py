47572d6f6f1479d67b6784ee60b6bbc2d280ea2e
drmiller1220 <drmiller@wustl.edu>
2016-08-15 19:26:29 -0500

    Improved Lab 4.  Still not wholly complete as intended (e.g. I have a random blank row between entries in the CSV that I cannot straighten out, I do not account for multiple specializations, etc.), but I am able to accommodate the specified attributes for each faculty member.

--- Day4/lab4.py
+++ Day4/lab4.py
@@ -1,5 +1,13 @@
 #Go to https://polisci.wustl.edu/faculty/specialization
 
+from bs4 import BeautifulSoup
+import urllib2 
+import random
+import time
+import os
+import re
+import csv
+
 web_address='https://polisci.wustl.edu/faculty/specialization'
 web_page = urllib2.urlopen(web_address)
 
@@ -46,23 +54,24 @@ for i in range(0, len(faculty_1)):
 		faculty=faculty_1[i]
 		member = faculty.find('a',{'class':"person-view-primary-field" })
 		member_name = member.get_text()
-		#member_name = member_name.replace('\n', ' ').replace('\r', '')
+		member_name = member_name.replace('\n', '')
 		member_page = "http://polisci.wustl.edu" + member['href']
 		member_title = faculty.get_text()
-		member_title = member_title.split("\n")
+		#member_title = member_title.split("\n")
 		member_title = member_title[2]
 		member_title = member_title.replace(' ', '')
-		#member_page = member_page.replace('\n', ' ').replace('\r', '')
+		#member_page = member_page.replace('\n', ' ')
 		member_page_proper = urllib2.urlopen(member_page)
 		member_page_proper = BeautifulSoup(member_page_proper.read())
-		#member_specialization = member_page_proper.find_all('a',{'property':"rdfs:label skos:prefLabel"})	
-		#for i in member_specialization:
-		#	if i.get_text() != 'Faculty':
-		#		member_specialization = i.get_text()
+		member_specialization = member_page_proper.find_all('a',{'property':"rdfs:label skos:prefLabel"})	
+		for i in member_specialization:
+			if i.get_text() != 'Faculty':
+				member_specialization = i.get_text()
+			else:
+				member_specialization.remove(i)
 		#page_email = member_page_proper.find_all('a',{'href':"mailto:"})
-		#for i in page_email:
-		#	if i.get_text() != 'polisci@wustl.edu':
-		#		page_email = i.get_text()
+		#if "mailto:" in member_page_proper['href'] and "polisci@wustl.edu" not in member_page_proper['href']:
+		#	page_email = page_email.get_text()
 		#page_website=member_page_proper.find_all('div',{'class':"field field-name-field-person-website field-type-link-field field-label-inline clearfix"})
 		#page_website=page_website.find('a')
 		#if page_website != '':
@@ -73,8 +82,8 @@ for i in range(0, len(faculty_1)):
 			with open('specializations.csv', 'w') as f:
 				my_writer = csv.DictWriter(f, fieldnames=("Name", "Specialization", "Title", "Email", "Web page"))
 				my_writer.writeheader()
-				my_writer.writerow({"Name":member_name, "Specialization": member_specialization, "Title": member_title, "Email": page_email, "Web page":member_page})
+				my_writer.writerow({"Name":member_name, "Specialization":member_specialization, "Web page":member_page})
 		else:
 			with open('specializations.csv', 'a+') as f:
 				my_writer = csv.DictWriter(f, fieldnames=("Name", "Specialization", "Title", "Email", "Web page"))
-				my_writer.writerow({"Name":member_name, "Specialization": member_specialization, "Title": member_title, "Email": page_email, "Web page":member_page})
+				my_writer.writerow({"Name":member_name, "Specialization":member_specialization, "Web page":member_page})
